cca590926840c78b4d5f3974a0424fb57a26278b
Shrivu Shankar <shrivu1122@gmail.com>
2019-09-07 13:25:44 -0500

    ✏️: Using Bits

--- wiress-server/src/main/java/AudioEncode.java
+++ wiress-server/src/main/java/AudioEncode.java
@@ -13,20 +13,63 @@ import javax.sound.sampled.AudioSystem;
 
 public class AudioEncode {
 
+    final static double sampleRate = 44_100;
+
     static void dataToWav(byte[] data, String filename) {
         try {
 
-            double sampleRate = 44100.0;
-            double amplitude = 1.0;
-            int samplesPerNote = (int) sampleRate / 4;
-            double seconds = data.length * samplesPerNote / sampleRate;
+            int samplesPerNote = (int) sampleRate / 16;
+            samplesPerNote = 1024;
+            double seconds = data.length * 8 * samplesPerNote / sampleRate;
 
             float[] buffer = new float[(int) (seconds * sampleRate)];
 
-            for (int i = 0; i < data.length; i++) {
-                int freq = data[i] * 50 + 200;
-                for (int sample = i * samplesPerNote; sample < (i + 1) * samplesPerNote - 1; sample++) {
-                    buffer[sample] = (float) (Math.sin(2 * Math.PI * sample * freq / sampleRate));
+            for (int i = 0; i < data.length * 8; i += 8) {
+                byte b = data[i / 8];
+                if((b & 1) != 0) {
+                    for (int sample = (i + 1) * samplesPerNote; sample < (i + 2) * samplesPerNote; sample++) {
+                        buffer[sample] = (float) (
+                                Math.sin(2 * Math.PI * sample * 2000 / sampleRate) +
+                                Math.sin(2 * Math.PI * sample * 1000 / sampleRate) + 
+                                Math.sin(2 * Math.PI * sample * 1500 / sampleRate)
+                        );
+                    }
+                }
+                if((b & 2) != 0) {
+                    for (int sample = (i + 2) * samplesPerNote; sample < (i + 3) * samplesPerNote; sample++) {
+                        buffer[sample] = (float) (
+                                Math.sin(2 * Math.PI * sample * 2000 / sampleRate) +
+                                Math.sin(2 * Math.PI * sample * 1000 / sampleRate) + 
+                                Math.sin(2 * Math.PI * sample * 1500 / sampleRate)
+                        );
+                    }
+                }
+                if((b & 4) != 0) {
+                    for (int sample = (i + 3) * samplesPerNote; sample < (i + 4) * samplesPerNote; sample++) {
+                        buffer[sample] = (float) (
+                                Math.sin(2 * Math.PI * sample * 2000 / sampleRate) +
+                                Math.sin(2 * Math.PI * sample * 1000 / sampleRate) + 
+                                Math.sin(2 * Math.PI * sample * 1500 / sampleRate)
+                        );
+                    }
+                }
+                if((b & 8) != 0) {
+                    for (int sample = (i + 4) * samplesPerNote; sample < (i + 5) * samplesPerNote; sample++) {
+                        buffer[sample] = (float) (
+                                Math.sin(2 * Math.PI * sample * 2000 / sampleRate) +
+                                Math.sin(2 * Math.PI * sample * 1000 / sampleRate) + 
+                                Math.sin(2 * Math.PI * sample * 1500 / sampleRate)
+                        );
+                    }
+                }
+                if((b & 16) != 0) {
+                    for (int sample = (i + 5) * samplesPerNote; sample < (i + 6) * samplesPerNote; sample++) {
+                        buffer[sample] = (float) (
+                                Math.sin(2 * Math.PI * sample * 2000 / sampleRate) +
+                                Math.sin(2 * Math.PI * sample * 1000 / sampleRate) + 
+                                Math.sin(2 * Math.PI * sample * 1500 / sampleRate)
+                        );
+                    }
                 }
             }
 
@@ -41,8 +84,7 @@ public class AudioEncode {
             File out = new File(filename);
             boolean bigEndian = false;
             boolean signed = true;
-            AudioFormat format;
-            format = new AudioFormat((float) sampleRate, 16, 1, signed, bigEndian);
+            AudioFormat format = new AudioFormat((float) sampleRate, 16, 1, signed, bigEndian);
             ByteArrayInputStream bais = new ByteArrayInputStream(byteBuffer);
             AudioInputStream audioInputStream;
             audioInputStream = new AudioInputStream(bais, format, buffer.length);
@@ -54,26 +96,24 @@ public class AudioEncode {
 
     static byte[] wavToBytes(String filename) {
         try {
-            WaveDecoder decoder = new WaveDecoder( new FileInputStream(filename) );
+            WaveDecoder decoder = new WaveDecoder(new FileInputStream(filename));
 
             float[] samples = new float[1024];
 
-            FFT fft = new FFT(1024, 44100);
-            
+            FFT fft = new FFT(1024, (float) sampleRate);
+
             ArrayList<Byte> datas = new ArrayList<>();
             byte last = -1;
 
             while (decoder.readSamples(samples) > 0) {
                 fft.forward(samples);
                 byte b = dataFromSpec(fft);
-                if(b != last) {
-                    datas.add(b);
-                    last = b;
-                }
+                System.out.print(b);
             }
-            
+            System.out.println();
+
             byte[] bytes = new byte[datas.size()];
-            for(int i = 0; i < datas.size(); i++) {
+            for (int i = 0; i < datas.size(); i++) {
                 bytes[i] = datas.get(i);
             }
             return bytes;
@@ -82,22 +122,36 @@ public class AudioEncode {
         }
         return null;
     }
-    
+
     static byte dataFromSpec(FFT fft) {
         float[] spectrum = fft.getSpectrum();
-        int best = -1;
-        float bestScore = -1;
-        for(int i = 0; i < spectrum.length; i++) {
-            if(spectrum[i] > bestScore) {
-                bestScore = spectrum[i];
-                best = i;
-            }
+//        int best = -1;
+//        float bestScore = -1;
+//        int minIndex = fft.freqToIndex(byteToFreq((byte)0));
+//        int maxIndex = fft.freqToIndex(byteToFreq((byte)127));
+//        for (int i = minIndex; i <= maxIndex; i++) {
+//            float freq = fft.indexToFreq(i);
+//            int nextIndex = fft.freqToIndex(freq + 500);
+//            float score = spectrum[i] * 5 + spectrum[nextIndex];
+//            if (score > bestScore) {
+//                bestScore = score;
+//                best = i;
+//            }
+//        }
+//        float freq = fft.indexToFreq(best);
+        int tIndex = fft.freqToIndex(2000);
+        if(spectrum[tIndex] > 0.1) {
+            return 1;
         }
-        float freq = fft.indexToFreq(best);
-        byte data = (byte)((freq - 200) / 50 + 0.5);
-        //String test = new String(new byte[]{data});
-        //System.out.println(best + " " + (int)((freq - 200) / 50 + 0.5) + "  " + test);
-        return data;
+        return 0;
+    }
+
+    static float byteToFreq(byte b) {
+        return b * 100 + 400;
+    }
+    
+    static byte freqToByte(float freq) {
+        return (byte) ((freq - 400) / 100 + 0.5);
     }
 
 }
--- wiress-server/src/main/java/MainServer.java
+++ wiress-server/src/main/java/MainServer.java
@@ -3,12 +3,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import net.bramp.ffmpeg.FFmpeg;
-import net.bramp.ffmpeg.FFmpegExecutor;
-import net.bramp.ffmpeg.FFprobe;
-import net.bramp.ffmpeg.builder.FFmpegBuilder;
 import static spark.Spark.*;
 import spark.*;
 
@@ -16,26 +10,31 @@ public class MainServer {
 
     final static String xmlTemplate = "<Response>\n"
             + "  <Start>\n"
-            + "      <Stream name=\"stream_USER_ID\" url=\"wss://216ed9e9.ngrok.io/voice\">\n"
+            + "      <Stream name=\"stream_1_USER_ID\" url=\"wss://216ed9e9.ngrok.io/voice\">\n"
             + "          <Parameter name=\"id\" value =\"USER_ID\"/>\n"
             + "      </Stream>\n"
             + "  </Start>\n"
             + "  <Play>https://216ed9e9.ngrok.io/resp/USER_ID</Play>\n"
-            + "  <Pause length=\"5\"/>\n"
+            + "  <Pause length=\"10\"/>\n"
             + "  <Stop>\n"
-            + "   <Stream name=\"stream_USER_ID\" />\n"
+            + "   <Stream name=\"stream_1_USER_ID\" />\n"
             + "  </Stop>"
             + "</Response>";
 
     public static void main(String[] args) {
+        // runServer();
+        byte[] data = "UU<html class='v2' dir='ltr' lang='en-GB' xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' ".getBytes();
+        AudioEncode.dataToWav(data, "out.wav");
+        byte[] dat = AudioEncode.wavToBytes("out.wav");
+        System.out.println(new String(dat));
+    }
+
+    public static void runServer() {
         port(8080);
         webSocket("/voice", WebSocketHandler.class);
         get("/resp/:id", (request, responce) -> getFile(request, responce));
         post("/xml", (request, responce) -> getXML(request, responce));
         init();
-//byte[] data = "<html class='v2' dir='ltr' lang='en-GB' xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' ".getBytes();
-//AudioEncode.dataToWav(data, "out.wav");
-//byte[] dat = AudioEncode.wavToBytes("out.wav");
     }
 
     private static Object getFile(Request request, Response response) {
